eb7f0256187c8d25909f5dc6f7e9e9a035ad15ef
foxidokun <fox@forestfox.tk>
2023-05-19 20:00:28 +0300

    run.sh -> compile.sh and remove compile logic from it

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/zsh
+
+if [ $# -l 3 ]; then
+    echo "Usage: ./compile.sh <AST file> <output bin path>"
+    exit
+fi
+
+filename=$(basename $1 .edoc)
+
+mkdir -p dump # for graphic dumps from frontend / middleend
+
+./bin/front               $1                      /tmp/$filename.ast
+./bin/middle              /tmp/$filename.ast      /tmp/$filename.opt.ast
+./bin/x64_compiler        /tmp/$filename.opt.ast  $2
+
+chmod +x $2
